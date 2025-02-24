8501234632b5a4a1ebd907643db116c8586c0259
NELUxP <zacherks@gmail.com>
2025-02-24 21:59:32 +0300

    somefixes 1111255141

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   // change the 'site' value to your site's domain
-  site: "https://neluxp.github.io/",
+  site: "https://neluxp.github.io/NELUxP/",
   image: {
     domains: ["raw.githubusercontent.com"],
   },
