1cc727a76b52444cfe3c3fa743e62a99ef9e6c39
NELUxP <zacherks@gmail.com>
2025-02-24 21:11:10 +0300

    added sss

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   // change the 'site' value to your site's domain
-  site: "https://nayasinghania.com",
+  site: "https://github.com/NELUxP/NELUxP.git",
   image: {
     domains: ["raw.githubusercontent.com"],
   },
