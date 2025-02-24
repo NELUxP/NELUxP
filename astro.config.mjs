f22340d96159c280aec4ef26cc5c52f01fd2387b
NELUxP <zacherks@gmail.com>
2025-02-24 21:36:05 +0300

    Update base URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,7 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 
-import tailwind from "@astrojs/tailwind";
-
-import sitemap from "@astrojs/sitemap";
-
-import partytown from "@astrojs/partytown";
-
 export default defineConfig({
-  // change the 'site' value to your site's domain
-  site: "https://github.com/NELUxP/NELUxP.git",
-  image: {
-    domains: ["raw.githubusercontent.com"],
-  },
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  base: "https://github.com/NELUxP/NELUxP.git", // Replace with your repository name
+  integrations: [react()],
 });
