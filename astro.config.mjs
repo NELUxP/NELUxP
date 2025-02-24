7e15f807c842afc74b34c22e4513e4379663ea93
NELUxP <zacherks@gmail.com>
2025-02-24 21:46:32 +0300

    somefixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,30 @@
+// @ts-check
 import { defineConfig } from "astro/config";
+
 import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
+
 export default defineConfig({
-  base: "https://github.com/NELUxP/NELUxP.git", // Replace with your repository name
-  integrations: [react()],
+  // change the 'site' value to your site's domain
+  site: "https://neluxp.github.io/",
+  image: {
+    domains: ["raw.githubusercontent.com"],
+  },
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
