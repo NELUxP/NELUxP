60ad095cd002602c21615d8c3c9b6f8b13a1bdfa
NELUxP <zacherks@gmail.com>
2025-02-26 19:46:13 +0300

    alot fiex

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   // change the 'site' value to your site's domain
-  site: "https://github.com/NELUxP/",
+  site: "https://mchax4u.netlify.app/",
   image: {
     domains: ["raw.githubusercontent.com"],
   },
